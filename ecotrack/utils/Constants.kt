f55aeb57a96a47191a5f56bb570709fcfa1464df
Jessica Pereira <157658256+jessiepsx@users.noreply.github.com>
2026-05-29 12:33:41 -0300

    Delete ecotrack directory

--- ecotrack/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.ecotrack.utils
-
-object Constants {
-    const val API_KEY = "f4a67cdc9a4322f414e1c0b0a6ba76c0"
-    const val UNITS = "metric"
-    const val LANGUAGE = "pt_br"
-    const val DEFAULT_LAT = -23.5489
-    const val DEFAULT_LON = -46.6388
-}
